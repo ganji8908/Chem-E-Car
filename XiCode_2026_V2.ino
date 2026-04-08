aa68d86a389b06fae5a7d13de0ca7b5858373ee1
Aditiven <37252836+Aditiven@users.noreply.github.com>
2026-04-08 14:17:26 -0700

    Update XiCode_2026_V2.ino

--- XiCode_2026_V2.ino
+++ XiCode_2026_V2.ino
@@ -61,7 +61,8 @@ void calibrateECScale() {  //probe
   
   calibrationSum = 0;
   for (int i = 0; i < 10; i++) {
-    calibrationReadings[i] = (uint32_t)analogRead(EC_pin)* 5000/1024;
+    //calibrationReadings[i] = (uint32_t)analogRead(EC_pin)* 5000/1024;
+    calibrationReadings[i] = (uint32_t)analogRead(EC_pin);
     analogArray[i] = calibrationReadings[i];
     calibrationSum += calibrationReadings[i];
     //delay(100);
@@ -145,7 +146,8 @@ void setup()
 
 void loop()
 {
-  rawValue = (uint32_t)analogRead(EC_pin)* 5000/1024; //reads voltage from probe (adjusted to 5V range)
+  //rawValue = (uint32_t)analogRead(EC_pin)* 5000/1024; //reads voltage from probe (adjusted to 5V range)
+  rawValue = (uint32_t)analogRead(EC_pin);
   switch_state = digitalRead(switch_pin); // stores switch position
 
   if (switch_state == 0) // Switch ON
@@ -202,7 +204,7 @@ void loop()
         regular_print(" Time: "); regular_print(currentTime);
         regular_print('\n');
         csv_print(currentTime); csv_print(","); csv_print(filterValue); csv_print('\n');
-        //delay(1000);
+        delay(100);
       }
   }
   else if (switch_state == 1) // Switch OFF
@@ -213,6 +215,7 @@ void loop()
 
     regular_print(" Analog Value: ");
     regular_print(rawValue);
+    delay(100);
     regular_println(" Switch: OFF ");
 
     // Extend linear actuator 
